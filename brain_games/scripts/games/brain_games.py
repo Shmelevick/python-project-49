8d25e8151fe8fb1508abc38bb54fe9655b65de90
shmelevik <yaroslav1992@gmail.com>
2025-03-14 15:12:23 +0300

    Update package

--- brain_games/scripts/games/brain_games.py
+++ brain_games/scripts/games/brain_games.py
@@ -1,18 +1,18 @@
 from brain_games.cli import welcome_user
-from brain_games.scripts.games.brain_calc import calc_game
-from brain_games.scripts.games.brain_even import is_even_game
-from brain_games.scripts.games.brain_gcd import gcd_game
-from brain_games.scripts.games.brain_prime import is_prime_game
-from brain_games.scripts.games.brain_progression import progression_game
+# from brain_games.scripts.games.brain_calc import calc_game
+# from brain_games.scripts.games.brain_even import is_even_game
+# from brain_games.scripts.games.brain_gcd import gcd_game
+# from brain_games.scripts.games.brain_prime import is_prime_game
+# from brain_games.scripts.games.brain_progression import progression_game
 
 
 def main():
     name = welcome_user()
-    is_even_game(name)
-    calc_game(name)
-    gcd_game(name)
-    progression_game(name)
-    is_prime_game(name)
+    # is_even_game(name)
+    # calc_game(name)
+    # gcd_game(name)
+    # progression_game(name)
+    # is_prime_game(name)
 
 
 if __name__ == "__main__":
